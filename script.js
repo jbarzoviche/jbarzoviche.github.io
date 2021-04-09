9b26a8a4ac9edd9a8a8829cd6bb3736dc6a8fcd4
James Barzoviche <jbarzoviche@gmail.com>
2021-04-08 20:59:01 -0500

    Estilos

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     new WOW().init();  
     var fullw = $(window).width(); 
     var hashed = window.location.hash;
+    
+    $('.main-content-wrapper').stickyStack();
 
     if(hashed){    
         $('html,body').animate({ scrollTop: ($(hashed).offset().top - 140) }, 500); 
@@ -12,6 +14,67 @@ $(document).ready(function(){
         hash = url.split('#')[1];
         var id = '#' + hash;
         $('html,body').animate({ scrollTop: ($(id).offset().top - 140) }, 500);      
+    });
+    
+    $(".content_experience_item").hover(function(){
+         ejecutar();
+    });
+
+
+    $("#cmodal_form").on("submit", function(e) {
+        e.preventDefault();
+        
+        var nombres = $("#nombres").val();
+        var correo = $("#correo").val();
+        var mensaje = $("#mensaje").val();
+                
+        var send = true;
+        $("#message").html('');        
+        
+        if(send && (nombres == '' || mensaje == '')){ 
+            $("#message").html('Llene todos los campos requeridos(*) del formulario');
+            send = false;
+        }
+        
+        if(send && !validateEmail(correo)){ 
+            $("#message").html('Ingrese un email válido');
+            send = false;
+        }
+        
+        if(send){   
+            
+            var data = {}; 
+            data['nombres'] = nombres; 
+            data['email'] = correo; 
+            data['content'] = mensaje; 
+        
+            $.ajax({ 
+                type: "POST",
+                headers: {
+                    'Authorization':'Bearer TGEgbXVjaGFjaGEgbWFsY3JpYWRh',
+                    'Content-Type':'application/json'
+                },
+                url: "https://dev.osuu.pe/rest/", 
+                dataType: "json", 
+                data: JSON.stringify(data), 
+                beforeSend: function() {
+                    $("#sendForm span").addClass("carga");
+                },
+                success: function(msg){ 
+                    $("#sendForm span").removeClass("carga");
+                    if(msg.status == '200 OK'){
+                        document.getElementById("cmodal_form").reset();
+                    }else{
+                        $("#message").html('Error al enviar');
+                    }
+                },
+                error: function (rpta, ajaxOptions, thrownError) {
+                    $("#sendForm span").removeClass("carga");
+                    
+                    $("#message").html('Error al enviar');
+                }
+            }); 
+        }
     });
 
     if (fullw <= 1200){        
@@ -27,88 +90,6 @@ $(document).ready(function(){
         }); 
 
     }
-    $("body").on("click",'.galeria_cc_item', function() {      
-        modal = $(this).data("modal");                               
-        idPost = $(this).data("id");                                                                      
-        $("." + modal).addClass("active");
-
-        $.ajax({
-            type : "post",
-            dataType: 'json',
-            url :  ajaxurl,
-            data : {
-                action: "getModalGallery",                
-                id : idPost                
-            },
-            beforeSend: function() {
-                $(".modal--galeria .contenedor").html('<div class="loading"><div class="loading_cc"><i class="fas fa-spinner"></i></div></div>');
-            },
-            success: function(data) {
-                $(".modal--galeria .contenedor").html(data.html);
-                
-                owl_galleria = jQuery('.owl_galleria');
-                owl_galleria.owlCarousel({
-                    nav: false,
-                    dots: true,
-                    loop:true,
-                    autoplay: false,
-                    autoplayHoverPause: false,
-                    onInitialized: counter,
-                    URLhashListener: true, 
-                    onTranslated: counter, 
-                    dotsContainer: '.owl_dots_galleria',
-                    items: 1
-                });
-            }
-        });
-        
-    });
-    
-    //close modal
-    $(".modal .close").click(function() {
-        menu = $(this).parent().parent();
-        menu.removeClass("active");        
-        $(".modal--galeria").html("");
-    });
-
-    $("[data-toggle='tab'] > ul > li").click(function(){
-        padre = $(this).parent().parent();
-        sub = $(".tab-panel", padre);
-        $(" > ul > li", padre).removeClass('active');
-        $(this).addClass('active');
-
-        if ($(this).hasClass('active')){
-            actual = $(this).data('tab');
-            sub.hide();
-            $('#' + actual).show();                        
-        }
-
-    });
-
-    $("[data-toggle='tab'] > ul > li.active").each(function(){   
-        actual = $(this).data('tab');
-        $('#' + actual).show();        
-    });
-
-    
-    $("[data-togg='tab'] > ul > li").click(function(){
-        padre = $(this).parent().parent();                
-        sub = $(".tab-panel-cc", padre);
-        $("> ul > li", padre).removeClass('active');
-        $(this).addClass('active');
-
-        if ($(this).hasClass('active')){
-            actual = $(this).data('tab');
-            sub.hide();
-            $('#' + actual).show();                        
-        }
-
-    });
-
-    $("[data-togg='tab'] > ul > li.active").each(function(){   
-        actual = $(this).data('tab');
-        $('#' + actual).show();        
-    });
 
     
     owl_banner_home = jQuery('.owl_banner_home');
@@ -150,16 +131,109 @@ function validaLetras(e){
     }
 } 
 
-function counter(event) {
-    var element = event.target;
-    var items = event.item.count;
-    var item = event.item.index + 1;
+function validateEmail($email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test( $email );
+}
 
-    if (item > items) {
-        item = item - items;
-    }
-    $(element)
-        .parent()
-        .find(".counter")
-        .html(item + " <span> / "+ items + "</span>");
+
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+		
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+			
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+			
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+			
+			$self.data('countTo', data);
+			
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+			
+			// initialize the element with the starting value
+			render(value);
+			
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+				
+				render(value);
+				
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+				
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+					
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+			
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.html(formattedValue);
+			}
+		});
+	};
+	
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 200,           // how long it should take to count between the target numbers
+		refreshInterval: 2,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+	
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+function ejecutar(){
+	jQuery(function ($) {
+	  // custom formatting example
+	  $('.count-number').data('countToOptions', {
+		formatter: function (value, options) {
+		  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})(?!\d))/g, ',');
+		}
+	  });
+	  
+	  // start all the timers
+	  $('.timer').each(count);  
+	  
+	  function count(options) {
+		var $this = $(this);
+		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+		$this.countTo(options);
+	  }
+	});
 }
